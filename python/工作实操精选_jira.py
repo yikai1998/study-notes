fb005e1eee0f35620e206f992f541f0ca7361af7
yikai <c1998828@icloud.com>
2026-04-16 18:02:31 +0800

    Update and rename jira调接口.py to 工作实操精选_jira.py

--- python/工作实操精选_jira.py
+++ python/工作实操精选_jira.py
@@ -1,58 +1,60 @@
+# coding = utf-8
 import requests
 from requests.auth import HTTPBasicAuth
 from jira import JIRA
-import json
-import ast
+import time
+import certifi
+
 
 # 基本config
 jira_server = 'https://airwallex.atlassian.net'
 jira_url = 'https://airwallex.atlassian.net/rest/api/3/search'
 jira_username = 'you jira account mail'
 jira_token = 'your jira account token'
-jira_auth = requests.auth.HTTPBasicAuth(username=jira_username, password=jira_token)
-jira_auth2 = JIRA(server=jira_server, basic_auth=(jira_username, jira_token))
+jira_basic_auth = HTTPBasicAuth(username=jira_username, password=jira_token)  # 给 requests 传的登录凭证
+jira_client = JIRA(server=jira_server, basic_auth=(jira_username, jira_token))  # Jira SDK client，对 Jira API 的封装入口。这是 jira Python 库的客户端对象。不是单纯“认证信息”，而是一个已经登录好的 Jira client。
 jira_headers = {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
 }
 
+
 # 用jql搜tickets
 jql_query = "PROJECT IN (CEOPS) AND issuetype in ('CN Inbound File Upload','Multiple GA') AND updated >= -5d ORDER BY created ASC"
 def jql_search(jql, batch_size: int = 100, limit: int = 1000):
     all_tickets = []
-    go_on = 1
     next_token = None
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
     url = 'https://airwallex.atlassian.net/rest/api/3/search/jql'
-    while go_on:
+    while True:
         payload = {
             'fields': ['summary','status', 'assignee', 'issuetype', 'customfield_11507'],
-            'fieldsByKeys': True,
+            'fieldsByKeys': True,  # 告诉 Jira：我这里传的是 key，请按 key 理解
             'jql': jql,
             'maxResults': batch_size,
             # 'startAt': begin  # 新版接口已不再用 startAt 分页，而是用 nextPageToken - 20250918
         }
         if next_token:
             payload['nextPageToken'] = next_token
-        response = requests.post(url=url, data=json.dumps(payload), headers=headers, auth=jira_auth).text
-        r = response.replace('true', 'True').replace('false', 'False').replace('null', 'None')
-        r = ast.literal_eval(r)
+        response = requests.post(url=url, json=payload, headers=jira_headers, auth=jira_basic_auth)
+        response.raise_for_status()
+        r = response.json()
         tickets = r.get('issues', [])
         next_token = r.get('nextPageToken', None)
         all_tickets.extend(tickets)
-        if any([limit - len(all_tickets) <= 0, not next_token]):
+        if len(all_tickets) >= limit:
+            all_tickets = all_tickets[:limit]
+            break
+        if not next_token:
             break
 
     return all_tickets
+
 
 # 通过key搜索该ticket的status transition history
 # 法1
 def get_status_change_history(issue_key):
     url = f"{jira_server}/rest/api/3/issue/{issue_key}?expand=changelog"
-    response = requests.get(url, headers=jira_headers, auth=jira_auth)
+    response = requests.get(url, headers=jira_headers, auth=jira_basic_auth)
     if response.status_code == 200:
         # Parse the JSON response
         issue_data = response.json()
@@ -73,27 +75,21 @@ def get_status_change_history(issue_key):
 
 # 法2
 def jira_transitions(key):
-    logs = config.jira.issue(key, expand='changelog').changelog.histories
+    logs = jira_client.issue(key, expand='changelog').changelog.histories
     logs = [
         [(log.created,act.fromString,act.toString) for act in log.items if act.field == 'status']
         for log in logs
     ]
     logs = [log for log in logs if log]
     return logs
-changeLogs = jira_transitions(key=id[0])
-new_data = ('', '', '')
-for hist in changeLogs:
-    if hist[0][2] == 'RFI':
-        new_data = hist[0]
-        break
-old_jira_df.loc[old_jira_df.Key == id[0], ['RfiTime', 'TransitionFrom', 'TransitionTo']] = new_data
 
-# update jira ticket status， 好像也可以同时commen ，暂未研究
-jira_auth2.transition_issue(key, 'Ask for URL/Doc')  # transition (str): ID or name of the transition to perform
+
+# update jira ticket status， 好像也可以同时comment，暂未研究
+jira_client.transition_issue(key, 'Ask for URL/Doc')  # transition (str): ID or name of the transition to perform
+
 
 # 评论ticket
-def comment_ticket(key):
-    wording = comment_wording
+def comment_ticket(key, wording):
     url = f'https://airwallex.atlassian.net/rest/api/latest/issue/{key}/comment'
     options = {
         "body": wording,
@@ -104,10 +100,14 @@ def comment_ticket(key):
             }
         }]
     }
-    requests.post(url=url, json=options, headers=jira_headers, verify=certifi.where(), allow_redirects=False, auth=jira_auth).json()
+    r = requests.post(url=url, json=options, headers=jira_headers, verify=certifi.where(), allow_redirects=False, auth=jira_basic_auth)
+    r.raise_for_status()
+    r = r.json()
+    return r
+
 
 # update jira fields, e.g.priority
-def jira_priority_highest(key, level='1'):
+def jira_update(key, assignee_id, reporter_id, participants_field_id, new_participants_ids, level='1'):
     url = f'https://airwallex.atlassian.net/rest/api/3/issue/{key}'
     payload = {
         'fields': {
@@ -117,42 +117,33 @@ def jira_priority_highest(key, level='1'):
             # 'customfield_12707': 'account_id_test1',
             # 'description': description_adf,
             'assignee': {
-              'id': new_assignee_id
+              'id': assignee_id
             },
             'reporter': {
-                'id': new_reporter_id
+                'id': reporter_id
             },
             participants_field_id: [
                 {'accountId': accountId} for accountId in new_participants_ids
-                # new_assignee_id = '5de7170cba60e10cfd67fa0e'
-                # new_reporter_id = '5de7170cba60e10cfd67fa0e'
-                # participants_field_id = 'customfield_10501'
-                # new_participants_ids = ['5de7170cba60e10cfd67fa0e', '617f433d327da400697a7bd0']
             ]
         }
     }
-    response = requests.put(
-        url,
-        json=payload,
-        headers=config.jira_headers,
-        auth=config.jira_auth
-    )
-    if response.status_code == 204:
-        print(f'Fields updated successfully for {key}')
-    else:
-        print(f'Failed to update fields with status code {response.status_code}: {response.content}')
-    time.sleep(0.1)
+    r = requests.put(url, json=payload, headers=jira_headers, auth=jira_basic_auth)
+    r.raise_for_status()  # 204 means fields updated successfully
+    return r.status_code
 '''
 the previous message will replace the current list of participants with the new list specified in the new_participants_ids. 
-If you want to add a participant without removing the existing ones, you would have to first get the current list of participants, add the new participant's ID to that list, and then update the field with the combined list
+If you want to add a participant without removing the existing ones, 
+you would have to first get the current list of participants, 
+add the new participant's ID to that list, 
+and then update the field with the combined list
 
 get_response = requests.get(
     url=f'https://airwallex.atlassian.net/rest/api/3/issue/{i}',
     headers=jira_headers,
-    auth=jira_auth
+    auth=jira_basic_auth
 )
 if get_response.status_code == 200:
-    current_participants = get_response.json()['fields'][participants_field_id]  # 可以很方便的找到特定key ticket的相应字段值
+    current_participants = get_response.json()['fields'].get(participants_field_id, [])  # 可以很方便的找到特定key ticket的相应字段值
     print(current_participants)
 else:
     print(f"Failed to get current participants: {get_response.content}")
@@ -191,6 +182,8 @@ new_issue_dict = {
     'description': 'created by python 0523',
     'issuetype': {'name': 'Task'}
 }
-new_issue = jira_auth2.create_issue(fields=new_issue_dict)
-# List of issues to be created. The create_issues method expects a list of dictionaries, where each dictionary represents the fields for an issue you want to create. The list can contain different types of issues or the same type with different data.
-new_issues = jira_auth2.create_issues(field_list=issue_list)
+new_issue = jira_client.create_issue(fields=new_issue_dict)
+# create jira tickets
+# The create_issues method expects a list of dictionaries, where each dictionary represents the fields for an issue you want to create.
+# The list can contain different types of issues or the same type with different data.
+new_issues = jira_client.create_issues(field_list=issue_list)
